24a622a6f8452a8202835966c2ec6e34792820c7
Prakhar Diwan <49339292+prakhardiwan@users.noreply.github.com>
2021-03-31 12:46:18 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -1,13 +1,12 @@
 #include<stdio.h>
 
+#define BLOCK_SIZE 8;
 
-
-void binaryOperatorOnTensor (Tensor* a, Tensor* b, Operation op){
-	// In-place binary operator: performs a = a op b 
+void binaryOperatorOnTensor (Tensor* a, Tensor* b, char op){
+	// In-place binary operator: performs a = a op b : element-wise
 	// supported op --> +,-,*,/ 
-	
 
-	// Possible issue: What if memory regions of the 2 tensors overlap : check condition required.. 
+	// $ Possible issue: What if memory regions of the 2 tensors overlap : check condition required.. 
 
 	// store tensor descriptors in local memory declared descriptors  
 	TensorDescriptor td_a = a.descriptor;
@@ -28,7 +27,27 @@ void binaryOperatorOnTensor (Tensor* a, Tensor* b, Operation op){
 		}
 	}
 
-	// Memory acccess; 
+	// Memory acccess: can be block wise OR element by element 
+	// $ here we do block-wise 
+
+	// $ Make sure no memory overlap is present amongst the tensor memories
+	
 
 
+	// performing operations: 
+	// $ we can check if op is supported or not before memory access for saving time 
+	switch(op){
+		case '+' : // a = a + b
+
+		case '-' : 
+
+		case '*' :
+
+		case '/' : 
+
+		default  : 
+			printf("%c operation not supported",op);
+			return;
 	}
+
+}
