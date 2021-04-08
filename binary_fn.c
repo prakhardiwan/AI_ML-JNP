bb7e2a55bbdb6bcb1e24b0b25d25eb2047656fd8
nayan0037 <46162513+nayan0037@users.noreply.github.com>
2021-04-08 22:29:24 +0530

    Binary operator testbench

--- binary_fn.c
+++ binary_fn.c
@@ -1,40 +1,39 @@
-#include<stdio.h>
 
-#define BLOCK_SIZE 8;
+#define BLOCK_SIZE 8
 
 
 // Cache
-uint32_t cache1[BLOCK_SIZE];
-uint32_t cache2[BLOCK_SIZE];
+unsigned int cache1[BLOCK_SIZE];
+unsigned int cache2[BLOCK_SIZE];
 
 void readMemPool(uint32_t cache[], MemPoolRequest* req){
-	uint32_t ptr = req.argument_2;
+	uint32_t* ptr = req->argument_2;
 	for(int i=0;i<BLOCK_SIZE;i=i+1){
-		cache[i] = *(ptr+i);		
+		cache[i] = *(ptr+i);
 	}
 }
 
 void writeMemPool(uint32_t cache[], MemPoolRequest* req){
-	uint32_t ptr = req.argument_2;
+	uint32_t* ptr = req->argument_2;
 	for(int i=0;i<BLOCK_SIZE;i=i+1){
-		*(ptr+i) = cache[i] ;		
+		*(ptr+i) = cache[i] ;
 	}
 }
 
 void binaryOperatorOnTensor (Tensor* a, Tensor* b, char op){
 	// In-place binary operator: performs a = a op b : element-wise
-	// supported op --> +,-,*,/ 
+	// supported op --> +,-,*,/
 
-	// $ Possible issue: What if memory regions of the 2 tensors overlap : check condition required.. 
-	// --> Assumed that non-overlapping memory regions 
+	// $ Possible issue: What if memory regions of the 2 tensors overlap : check condition required..
+	// --> Assumed that non-overlapping memory regions
 
 	// $ Possible issue: Typecasting required for hw model || Universal Cache for all data types
-	// --> Assumed datatype 
+	// --> Assumed datatype
 
 	// $ Possible issue: cache structure defn
 	// --> we have assumed 2D for operation; and one dimension = BLOCK_SIZE
 
-	// $ Possible issue: 1 vector in row-major format and another vector col major format 
+	// $ Possible issue: 1 vector in row-major format and another vector col major format
 	// [a a]
 	// [b b]
 	// [c c] ---> in row-major rep
@@ -45,84 +44,92 @@ void binaryOperatorOnTensor (Tensor* a, Tensor* b, char op){
 	// [c]
 	// [c]
 
-	// store tensor descriptors in local memory declared descriptors  
-	TensorDescriptor td_a = a.descriptor;
-	TensorDescriptor td_b = b.descriptor;
+	// store tensor descriptors in local memory declared descriptors
+	TensorDescriptor td_a = a->descriptor;
+	TensorDescriptor td_b = b->descriptor;
 	// Number of dimensions
 	uint32_t a_num_dim = td_a.number_of_dimensions;
 	uint32_t b_num_dim = td_b.number_of_dimensions;
 
 	// Total iterations
-	uint32_t max_iter = 1;
-	// Tensor dimensions match check 
+	uint32_t max_iter = 1,flat_dims=1;
+	// Tensor dimensions match check
 	if(a_num_dim!= b_num_dim){
 		printf("Tensors incompatible: Number of dimensions don't match");
 		return;
 	}
 	else{
-		int n_dim = td_a.number_of_dimensions();
+		int n_dim = td_a.number_of_dimensions;
 		for(int i=0; i<n_dim; i=i+1){
 			if(td_a.dimensions[i]!=td_b.dimensions[i]){
 				printf("Tensors incompatible: Atleast one of the dimensions %d don't match", i);
 				return;
 			}
-			max_iter *= td_a.dimensions[i]; 
+			flat_dims *= td_a.dimensions[i];
 		}
-		max_iter /= BLOCK_SIZE; 
-		if(max_iter==0){
-			max_iter = 1;
-		}
+		max_iter = ceil( ((float)flat_dims) / BLOCK_SIZE) ;
+
+		//printf("Max iter = %d\n",max_iter);
 	}
 
-	// Memory acccess: can be block wise OR element by element 
-	// $ here we do block-wise 
+	// Memory acccess: can be block wise OR element by element
+	// $ here we do block-wise
 
 	// $ Make sure no memory overlap is present amongst the tensor memories
 
-	// performing operations: 
-	// $ we can check if op is supported or not before memory access for saving time 
-	
+	// performing operations:
+	// $ we can check if op is supported or not before memory access for saving time
 
+
+    uint32_t dims = 0;
 	switch(op){ // for request_type: read --> 2; write -->3
 		case '+' : // a = a + b
 			for(int i=0; i<max_iter; i=i+1){
-				MemPoolRequest* req_a;
+
+				MemPoolRequest req_a;
 				req_a.request_type = 2;
 				req_a.request_tag = i; //
 				req_a.argument_0 = BLOCK_SIZE; //assumed datatype/size
-				req_a.argument_1 = a.mem_pool_identifier;
-				req_a.argument_2 = a.mem_pool_buffer_pointer + BLOCK_SIZE*i; // data size to be incorporated
+				req_a.argument_1 = a->mem_pool_identifier;
+				req_a.argument_2 = a->mem_pool_buffer_pointer + BLOCK_SIZE*i*4; // data size to be incorporated
 
-				MemPoolRequest* req_b;
+				MemPoolRequest req_b;
 				req_b.request_type = 2;
 				req_b.request_tag = i; //
 				req_b.argument_0 = BLOCK_SIZE; //assumed datatype/size
-				req_b.argument_1 = b.mem_pool_identifier;
-				req_b.argument_2 = b.mem_pool_buffer_pointer + BLOCK_SIZE*i;
+				req_b.argument_1 = b->mem_pool_identifier;
+				req_b.argument_2 = b->mem_pool_buffer_pointer + BLOCK_SIZE*i*4; // data size to be incorporated
 
-				readMemPool(cache1,req_a);	
-				readMemPool(cache2,req_b);
-				
-				for(int j=0; j<BLOCK_SIZE; j=j+1){ //typecasting problem
+				readMemPool(cache1,&req_a);
+				readMemPool(cache2,&req_b);
+
+                uint32_t k;
+                if(dims+BLOCK_SIZE > flat_dims){
+                    k = flat_dims - dims;
+                }else{
+                    k = BLOCK_SIZE;
+                }
+                //printf("k = %d\n",k);
+				for(int j=0; j<k; j=j+1){ //typecasting problem
 					cache1[j] = cache1[j] + cache2[j];
 				}
 				req_a.request_type = 3;
-				writeMemPool(cache1,req_a);
+				writeMemPool(cache1,&req_a);
+				dims += BLOCK_SIZE;
 			}
 			break;
-		case '-' : 
+		case '-' :
 			break;
 
 		case '*' :
 			break;
 
-		case '/' : 
+		case '/' :
 			break;
 
-		default  : 
+		default  :
 			printf("%c operation not supported",op);
 			return;
 	}
 
 }
-
---
+++ binary_tb.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include "../primitives/include/mempool.h"
+#include "../primitives/include/tensor.h"
+#include "binary_fn.c"
+
+//assuming only read
+void memPoolAccess (MemPoolRequest* req, MemPoolResponse* resp){
+	if(req->request_type = 2){
+		uint32_t mem_pointer = req->argument_1;
+		// not needed to complete
+		// as we can assume mempool has
+		// transfered the req bytes in
+		// cache. So directly read from
+		// buffer pointer;
+
+	}
+}
+Tensor createTensor (uint32_t ndim, uint32_t* dims, TensorDataType dt, uint16_t mempool){
+	TensorDescriptor td;
+	Tensor tensor;
+	uint32_t mem_pool_size = 1;
+
+	td.row_major_form = 1;
+	td.number_of_dimensions = ndim;
+
+	for (int i = 0; i < ndim; ++i)
+	{
+		td.dimensions[i] = dims[i];
+		mem_pool_size = mem_pool_size * dims[i] * 4; //size_of(dt) map from enum
+	}
+
+	tensor.descriptor = td;
+
+	uint32_t* mem_pointer;
+
+	mem_pointer = malloc(mem_pool_size);
+
+	//initialize tensor with some values
+	for (int i = 0; i < mem_pool_size/4; ++i)
+	{
+		mem_pointer[i] = i+mempool;
+	}
+
+	tensor.mem_pool_identifier = mempool;
+	tensor.mem_pool_buffer_pointer = mem_pointer;
+
+	return tensor;
+}
+
+void main(){
+	const uint32_t ndim  = 2;
+	uint32_t dims[ndim];
+	dims[1] = 5;
+	dims[0] = 5;
+
+	//define tensor A
+
+	Tensor A,B;
+	A = createTensor(ndim, dims, u32,0);
+
+	//define tensor B
+	B = createTensor(ndim, dims, u32,1);
+
+	//call function
+	binaryOperatorOnTensor(&A,&B,'+');
+
+
+	//check B
+	uint32_t mem_pool_size=1;
+	for (int i = 0; i < ndim; ++i)
+	{
+		mem_pool_size = mem_pool_size * dims[i]; //size_of(dt) map from enum
+	}
+	uint32_t* mem = A.mem_pool_buffer_pointer;
+	for (int i = 0; i < mem_pool_size; ++i)
+	{
+		if(mem[i] == 1+2*i){
+
+		}else{
+			printf("Error at %d  value = %d\n",i,mem[i]);
+			break;
+		}
+	}
+}
